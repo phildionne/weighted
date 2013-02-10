873c51b273fb119e576ad28a3be858f3e1d90957
Philippe Dionne <dionne.phil@gmail.com>
2013-02-10 18:12:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weighted::Application.config.secret_token = '295ee8b9bbd45c571b9c16c8e471e9d31b3a5245b31eaad70c336f06c36352454d8f9e5d8d757d760f1ebbb4ac969a77d952e99799b7ab9bddab35804e0fbce9'
