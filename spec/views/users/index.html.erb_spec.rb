4bd86aa77fddf5fa6ca32e0b17f1f868b1d2c374
Philippe Dionne <dionne.phil@gmail.com>
2013-02-10 21:33:20 -0500

    rails generate scaffold user username:string

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "users/index" do
+  before(:each) do
+    assign(:users, [
+      FactoryGirl.create(:user),
+      FactoryGirl.create(:user)
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Username".to_s, :count => 2
+  end
+end
