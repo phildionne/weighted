d934de2a4dea9a933bdea63e9194bb2b864a8338
Philippe Dionne <dionne.phil@gmail.com>
2013-02-10 20:09:33 -0500

    Add page_title and body_class helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  # Extracted from https://github.com/thoughtbot/flutie/blob/master/app/helpers/page_title_helper.rb
+  def page_title(options = {})
+    app_name = options[:app_name] || Rails.application.class.to_s.split('::').first
+    page_title_symbol = options[:page_title_symbol] || :page_title
+    separator = options[:separator] || ' : '
+
+    if content_for?(page_title_symbol)
+      [app_name, content_for(page_title_symbol)].join(separator)
+    else
+      app_name
+    end
+  end
+
+  # Extracted from https://github.com/thoughtbot/flutie/blob/master/app/helpers/body_class_helper.rb
+  def body_class(options = {})
+    extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+
+    if content_for?(extra_body_classes_symbol)
+      [basic_body_class, content_for(extra_body_classes_symbol)].join(' ')
+    else
+      basic_body_class
+    end
+  end
 end
