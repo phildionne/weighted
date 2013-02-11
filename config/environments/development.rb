d9a9ed70e40d803e444ca4745934955790d8f57f
Philippe Dionne <dionne.phil@gmail.com>
2013-02-10 19:25:01 -0500

    Use foreman

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 Weighted::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Disable Ruby from buffering stdout so
+  # Foreman will display to the terminal output correctly
+  $stdout.sync = true
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
