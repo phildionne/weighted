3190c2132370d166961963586ba538b68c931d31
Philippe Dionne <dionne.phil@gmail.com>
2013-02-16 18:53:33 -0500

    Add validations

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,9 @@
 class Content < ActiveRecord::Base
   attr_accessible :body, :source, :title
+
+  validates_presence_of     :body, :source, :title
+  validates_uniqueness_of   :source, :title
+  validates_length_of       :title, :maximum => 255, :allow_blank => false, :too_long => 'less than 255 characters'
+
+  before_save { self.title = self.title.titleize }
 end
