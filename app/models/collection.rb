bbc4599e3393fce9a7af8c8eebcbd25c15a06b13
Philippe Dionne <dionne.phil@gmail.com>
2013-02-16 12:34:42 -0500

    Add validations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,9 @@
 class Collection < ActiveRecord::Base
   attr_accessible :description, :name
+
+  validates_presence_of :description, :name
+  validates_uniqueness_of :description, :name
+  validates_length_of :name, :maximum => 255, :too_long => 'less than 255 characters'
+
+  before_save { self.name = self.name.titleize }
 end
