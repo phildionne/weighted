9f96574298384f40801872620435f2b3c317bbb5
Philippe Dionne <dionne.phil@gmail.com>
2013-02-18 21:48:57 -0500

    Fix contents routing specs

--- spec/routing/contents_routing_spec.rb
+++ spec/routing/contents_routing_spec.rb
@@ -4,31 +4,31 @@ describe ContentsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/contents").should route_to("contents#index")
+      get("/collections/:collection_id/contents").should route_to("contents#index", :collection_id => ":collection_id")
     end
 
     it "routes to #new" do
-      get("/contents/new").should route_to("contents#new")
+      get("/collections/:collection_id/contents/new").should route_to("contents#new", :collection_id => ":collection_id")
     end
 
     it "routes to #show" do
-      get("/contents/1").should route_to("contents#show", :id => "1")
+      get("/collections/:collection_id/contents/1").should route_to("contents#show", :collection_id => ":collection_id", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/contents/1/edit").should route_to("contents#edit", :id => "1")
+      get("/collections/:collection_id/contents/1/edit").should route_to("contents#edit", :collection_id => ":collection_id", :id => "1")
     end
 
     it "routes to #create" do
-      post("/contents").should route_to("contents#create")
+      post("/collections/:collection_id/contents").should route_to("contents#create", :collection_id => ":collection_id")
     end
 
     it "routes to #update" do
-      put("/contents/1").should route_to("contents#update", :id => "1")
+      put("/collections/:collection_id/contents/1").should route_to("contents#update", :collection_id => ":collection_id", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/contents/1").should route_to("contents#destroy", :id => "1")
+      delete("/collections/:collection_id/contents/1").should route_to("contents#destroy", :collection_id => ":collection_id", :id => "1")
     end
 
   end
