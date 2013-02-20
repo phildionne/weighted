ff2b094cfaf4a78818b4e04b1cfe8b8d91b4237a
Philippe Dionne <dionne.phil@gmail.com>
2013-02-20 17:27:36 -0500

    Fix indent

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,9 +3,9 @@ class Collection < ActiveRecord::Base
 
   has_many :contents
 
-  validates_presence_of :description, :name
-  validates_uniqueness_of :description, :name
-  validates_length_of :name, :maximum => 255, :too_long => 'less than 255 characters'
+  validates_presence_of     :description, :name
+  validates_uniqueness_of   :description, :name
+  validates_length_of       :name, :maximum => 255, :too_long => 'less than 255 characters'
 
   before_save { self.name = self.name.titleize }
 end
