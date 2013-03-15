75acb0e5f7f04b317e5ace495042b40413a65deb
Philippe Dionne <dionne.phil@gmail.com>
2013-03-14 23:17:11 -0400

    Add User and Collection with contents seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p "--- Seeding users"
+20.times do
+  FactoryGirl.create(:user)
+end
+
+p "--- Seeding collections with contents"
+40.times do
+  FactoryGirl.create(:collection_with_contents)
+end
