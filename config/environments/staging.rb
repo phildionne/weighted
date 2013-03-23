733ccb68a809771406f2a92fe64013ccaca99dde
Philippe Dionne <dionne.phil@gmail.com>
2013-03-23 12:07:52 -0400

    Configure assets to be served through a Cloudfront distribution

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,7 +43,7 @@ Weighted::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "https://d10qu47grxb9bh.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
