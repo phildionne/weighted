ca8f26ca4ddda5ed3d7cfdfe9c99b630939b2323
Philippe Dionne <dionne.phil@gmail.com>
2013-04-14 16:47:57 -0400

    Add RegistrationController and OmniauthController placeholder specs

---
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Users::OmniauthCallbacksController do
+  include Devise::TestHelpers
+
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+  end
+
+  describe "#all" do
+    it "updates the user's profile with auth provider informations" do
+      pending
+    end
+  end
+end
