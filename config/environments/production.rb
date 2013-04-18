102087744dc8cda040003f338f3bfd4c272f916f
Philippe Dionne <dionne.phil@gmail.com>
2013-04-17 21:04:18 -0400

    Move asset_host configuration into an environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Weighted::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://d2y5pqdeoz9ati.cloudfront.net"
+  config.action_controller.asset_host = "//#{ENV['ASSET_HOST']}"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
