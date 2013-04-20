da08ce0a1543114243b33bbf98c1410f0c4f23b2
Philippe Dionne <dionne.phil@gmail.com>
2013-04-20 09:44:18 -0400

    Give routes indentation some :heart:

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Weighted::Application.routes.draw do
   end
 
   devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
+    :registrations      => 'users/registrations',
     :omniauth_callbacks => 'users/omniauth_callbacks'
   }
   devise_scope :user do
     # Not added by default while using devise with omniauth
-    delete '/users/sign_out' => 'devise/sessions#destroy'
-    get 'settings/account' => 'devise/registrations#edit'
+    delete '/users/sign_out'   => 'devise/sessions#destroy'
+    get    'settings/account'  => 'devise/registrations#edit'
   end
   resources :users, :only => :show
 
@@ -19,7 +19,7 @@ Weighted::Application.routes.draw do
     resources :contents
   end
 
-  get 'pages/about' => 'high_voltage/pages#show', id: 'about'
+  get 'pages/about'   => 'high_voltage/pages#show', id: 'about'
   get 'pages/contact' => 'high_voltage/pages#show', id: 'contact'
 
   root :to => 'high_voltage/pages#show', id: 'frontpage'
