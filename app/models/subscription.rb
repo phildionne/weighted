5de32f9218147a55d51c61cad5cffd363b07e03e
Philippe Dionne <dionne.phil@gmail.com>
2013-05-05 09:24:15 -0400

    Create Stripe customer in Subscription model callbacks

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,4 +6,31 @@ class Subscription < ActiveRecord::Base
 
   validates :user, presence: true
   validates :stripe_customer_id, presence: true, on: :update
+  before_validation :create_stripe_customer, on: :create
+  before_destroy    :destroy_stripe_customer
+  private
+
+  # Creates a Stripe customer, raises on failure
+  def create_stripe_customer
+    begin
+      customer = Stripe::Customer.create(email: user.email,
+                                         plan:  'default')
+      self.stripe_customer_id = customer.id
+    rescue Stripe::StripeError => e
+      logger.error e
+      raise e
+    end
+  end
+
+  # Destroys a Stripe customer, raises on failure
+  def destroy_stripe_customer
+    begin
+      customer = Stripe::Customer.retrieve(stripe_customer_id)
+      customer.delete
+    rescue Stripe::StripeError => e
+      logger.error e
+      raise e
+    end
+  end
+
 end
