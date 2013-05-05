2effef5018f85d02a5d9692803fac30605b797eb
Philippe Dionne <dionne.phil@gmail.com>
2013-05-05 13:55:52 -0400

    Add SubscriptionsController and frontend logic

---
+++ app/assets/javascripts/subscription.js
@@ -0,0 +1,32 @@
+Stripe.setPublishableKey($('meta[name="stripe-publishable-key"]').attr('content'));
+
+var stripeResponseHandler = function(status, response) {
+  var $form = $('#payment-form');
+
+  if (response.error) {
+    // Show the errors on the form
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('input[type=submit]').prop('disabled', false);
+  } else {
+    // token contains id, last4, and card type
+    var token = response.id;
+    // Insert the token into the form so it gets submitted to the server
+    $form.find($('#stripe_card_token').val(token));
+    // and re-submit
+    $form.get(0).submit();
+  }
+};
+
+jQuery(function($) {
+  $('#payment-form').submit(function(e) {
+    var $form = $(this);
+
+    // Disable the submit button to prevent repeated clicks
+    $form.find('input[type=submit]').prop('disabled', true);
+
+    Stripe.createToken($form, stripeResponseHandler);
+
+    // Prevent the form from submitting with the default action
+    return false;
+  });
+});
