4f543adcedd7b36ad008ed4f968cce85f898fc2a
Philippe Dionne <dionne.phil@gmail.com>
2013-05-14 20:32:04 -0400

    Configure vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,8 +3,23 @@
 
 require 'vcr'
 
-VCR.configure do |c|
-  # c.default_cassette_options = { :re_record_interval => 86400 }
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
-  c.hook_into :webmock
+VCR.configure do |vcr|
+  vcr.default_cassette_options = { record: :once, re_record_interval: 1.day }
+  vcr.cassette_library_dir = 'spec/fixtures/cassettes'
+  vcr.hook_into :webmock
+  vcr.configure_rspec_metadata!
+
+  vcr.around_http_request do |request|
+
+    if request.uri =~ /api.stripe.com/
+      uri = URI(request.uri)
+      name = "#{[uri.host, uri.path, request.method].join('/')}"
+      VCR.use_cassette(name, &request)
+
+    elsif request.uri =~ /twitter.com/
+      VCR.use_cassette('twitter', &request)
+    else
+    end
+
+  end
 end
