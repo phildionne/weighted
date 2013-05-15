a588b6a2e17e85ec14b29d4a18389cf67939e39f
Philippe Dionne <dionne.phil@gmail.com>
2013-05-14 21:46:31 -0400

    Complete Omniauth specs support

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -4,8 +4,26 @@
 # Turn on "test mode" for OmniAuth
 OmniAuth.config.test_mode = true
 
-# Create a default auth hash
+# Complete auth hash
 OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+  :provider => 'twitter',
+  :uid => '12345',
+  :info => {
+    "nickname" => "Tester",
+    "name" => "Tester",
+    "location" => "Test City",
+    "email" => "test@example.com",
+    "image" => "http://a0.twimg.com/profile_images/test.jpeg",
+    "description" => "Test description",
+    "urls" => {
+      "Website" => "http://test.com/",
+      "Twitter" => "http://twitter.com/test"
+    }
+  }
+})
+
+# Incomplete auth hash (missing email)
+OmniAuth.config.mock_auth[:twitter_incomplete] = OmniAuth::AuthHash.new({
   :provider => 'twitter',
   :uid => '12345',
   :info => {
