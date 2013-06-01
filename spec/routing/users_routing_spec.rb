24d2037337a626b81556636cb55b353ae3119d63
Philippe Dionne <dionne.phil@gmail.com>
2013-06-01 19:15:58 -0400

    Add collections #followers and users #followed_collections routing specs

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -7,5 +7,9 @@ describe UsersController do
       get("/users/1").should route_to("users#show", :id => "1")
     end
 
+    it "routes to #followed_collections" do
+      get("/users/1/followed_collections").should route_to("users#followed_collections", :id => "1")
+    end
+
   end
 end
