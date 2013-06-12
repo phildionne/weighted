498fa803273437df2c16285219ee36029a2d1eeb
Philippe Dionne <dionne.phil@gmail.com>
2013-06-11 21:30:05 -0400

    Use #belongs_to in migrations

--- db/migrate/20130323202138_create_collection_source_join_table.rb
+++ db/migrate/20130323202138_create_collection_source_join_table.rb
@@ -1,8 +1,8 @@
 class CreateCollectionSourceJoinTable < ActiveRecord::Migration
   def change
     create_table :collections_sources, id: false do |t|
-      t.integer :collection_id
-      t.integer :source_id
+      t.belongs_to :collection
+      t.belongs_to :source
     end
   end
 end
