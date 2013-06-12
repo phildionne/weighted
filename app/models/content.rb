b48f1c6215d50ae23d18267f52ae08d9cf06af61
Philippe Dionne <dionne.phil@gmail.com>
2013-06-11 21:48:10 -0400

    Sanitize Content #body html

--- app/models/content.rb
+++ app/models/content.rb
@@ -10,6 +10,7 @@ class Content < ActiveRecord::Base
   validates :title,                     length: { minimum: 3, maximum: 255, allow_blank: false }
 
   before_save :titleize_title
+  before_save :sanitize_body
 
   private
 
@@ -17,4 +18,14 @@ class Content < ActiveRecord::Base
     self.title = self.title.titleize
   end
 
+  # Sanitizes html content
+  #
+  # @note Allows a variety of markup including formatting tags, links, lists and tables.
+  # @note Images are limited to HTTP and HTTPS, links are limited to FTP, HTTP, HTTPS, and mailto protocols.
+  # @see Full configuration here https://github.com/rgrove/sanitize/blob/master/lib/sanitize/config/relaxed.rb
+  def sanitize_body
+    # TODO: Remove from the output the following node elements: data-*, id, class, onmouseout (and others), trackbacks, comment form
+    # TODO: Handle code blocks and syntax highlighting
+    Sanitize.clean(body, Sanitize::Config::RELAXED)
+  end
 end
