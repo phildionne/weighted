72ffbef33aaed8b552b851ded31f80a58d023bd9
Philippe Dionne <dionne.phil@gmail.com>
2013-06-16 21:54:56 -0400

    Add subscribe and unsubscribe to Feed model

--- app/models/collections/feed.rb
+++ app/models/collections/feed.rb
@@ -10,4 +10,28 @@ class Feed < Collection
   attr_accessible :feed_url
 
   validates :feed_url, presence: true
+
+  after_initialize :assign_uuid
+  after_create     :create_feed_subscription
+  after_destroy    :destroy_feed_subscription
+
+  private
+
+  def assign_uuid
+    # UUID
+  end
+
+  # Creates the associated feed subscription on the 3rd party service
+  #
+  # @return [Boolean]
+  def create_feed_subscription
+    Weighted::Application::Superfeedr.subscribe(feed_url)
+  end
+
+  # Destroys the associated feed subscription on the 3rd party service
+  #
+  # @return [Boolean]
+  def destroy_feed_subscription
+    Weighted::Application::Superfeedr.unsubscribe(feed_url)
+  end
 end
