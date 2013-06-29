3247c165e166b360bb58327fda824b3ac2cab0b3
Philippe Dionne <dionne.phil@gmail.com>
2013-06-29 14:57:54 -0400

    Upgrade config files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Weighted::Application.config.secret_token = '295ee8b9bbd45c571b9c16c8e471e9d31b3a5245b31eaad70c336f06c36352454d8f9e5d8d757d760f1ebbb4ac969a77d952e99799b7ab9bddab35804e0fbce9'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weighted::Application.config.secret_key_base = '67ec60468f88817f1f83f258e67a5e96ba85dc47fdeeb28080a3a7147bba7c16840d6d47f051a15cecbd30070b9601e7241503a77589f740c5d3a9594e196b85'
